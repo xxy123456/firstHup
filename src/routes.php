5148524c641aca9f04664f25e9ddfec61e309a26
hooklife <miaogaopeng123@gmail.com>
2018-11-26 16:25:25 +0800

    fix psr

--- src/routes.php
+++ src/routes.php
@@ -2,18 +2,14 @@
 
 use Slim\Http\Request;
 use Slim\Http\Response;
-use Controllers\TestController;
+
 // Routes
 
 $app->get('/', function (Request $request, Response $response, array $args) {
-
     $this->logger->info("zhaozhikai '/' route");
     echo 'hello,world';
-    
-
 });
+
 $app->get('/index', function (Request $request, Response $response, array $args) {
-
     echo phpinfo();
-
 });
